eff96a16400f5355bb73916db80c31b6ca30f9af
Go Blue JuJu <116537881+JuJu9999@users.noreply.github.com>
2022-10-24 16:46:07 +0900

    Update python_condition.py
    Conditional phrases

---
+++ python_condition.py
@@ -0,0 +1,46 @@
+# number = int(input())
+
+# if number < 5:
+    # print("숫자가 5보다 작다.")
+# elif 5 < number < 10
+    # print("5보다 크고 10보다 작다")
+# else: 
+    # print("10보다 큽니다")
+
+
+
+# money = int(input())
+# if money > 7:
+    # print("비행기 타자")
+# elif money > 5:
+    # print("버스를 타자")
+# else:
+    # print("걷자")
+
+# countries = ["kor", "Japan", "Thai","usa"]
+# for country in countries:
+    # if country == "kor":
+        # print("한글로 번역")
+    # elif country == "usa":
+        # print("영어로 번역") 
+    # else:
+        # print("중국어로 번역")
+
+# a = 0
+# while a < 5:
+    # a = a + 1
+    # print(a)
+
+
+# result = 0
+# for i in range(1,3):
+    # print(i)
+    # result == result + 1
+    # print(result)
+
+for i in range(10):
+    if 3<=i<=5:
+        continue
+        # break 멈추기
+
+    print(i)
