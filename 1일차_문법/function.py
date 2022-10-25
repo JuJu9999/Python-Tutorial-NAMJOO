f2666995809fc354531a0bcc07286dd768cc1162
Go Blue JuJu <116537881+JuJu9999@users.noreply.github.com>
2022-10-25 17:28:35 +0900

    crawling first attempt

---
+++ 1일차_문법/function.py
@@ -0,0 +1,32 @@
+
+
+# def sum(a,b):
+    # print("a+b")
+    # print(a+b)
+    # return a+b
+
+# total = sum(3,4)
+# print(total)
+# It is a function
+
+
+# def check_grade(성적, 이름):
+#     if 성적 >= 90:
+#         grade = 'A'
+#         print(이름 + "의 등급은 A 입니다.")
+#     elif 성적 >= 80:
+#         grade = 'B'
+#         print(이름 + "의 등급은 B 입니다.")
+#     elif 성적 >= 70:
+#         grade = 'C'
+#         print(이름 + "의 등급은 C 입니다.")
+#     elif 성적 >= 60: 
+#         grade = 'D' 
+#         print(이름 + "의 등급은 D 입니다.")
+#     else:
+#         grade = "F"
+#         print(이름 + "의 등급은 F 입니다.")
+   
+
+# a = check_grade(100, 'namjoo')
+
